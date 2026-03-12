62d31e9a5ee1b4df5d633e69d8f51e2b705eefcc
j <j>
2026-03-12 09:16:15 -0500

    gjkfadls

--- Main.java
+++ Main.java
@@ -12,11 +12,23 @@ public class Main {
     }
 
     public static void mergeSort(int[] array) {
-        // Your code here
+        if (array.length < 2) return;
+        int mid = array.length / 2;
+        int[] left = new int[mid];
+        int[] right = new int[array.length - mid];
+        System.arraycopy(array, 0, left, 0, mid);
+        System.arraycopy(array, mid, right, 0, array.length - mid);
+        mergeSort(left);
+        mergeSort(right);
+        merge(array, left, right);
     }
 
     public static void merge(int[] array, int[] left, int[] right) {
-        // Your code here
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length)
+            array[k++] = left[i] < right[j] ? left[i++] : right[j++];
+        while (i < left.length) array[k++] = left[i++];
+        while (j < right.length) array[k++] = right[j++];
     }
 
     public static void printArray(int[] array) {
